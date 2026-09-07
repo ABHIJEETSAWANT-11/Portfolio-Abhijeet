299eecb1f779ebfcf021bb67bc85895095b44add
ABHIJEETSAWANT-11 <abhijeetsawant99709@gmail.com>
2026-09-07 21:37:07 +0530

    Update hamburger.js

--- JavaScript/hamburger.js
+++ JavaScript/hamburger.js
@@ -8,6 +8,10 @@ navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open' );
 });
 
+
+
+
+
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
